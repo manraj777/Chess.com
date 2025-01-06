4446a7035259cdda90d69d12d6baa100cd573a9c
manraj777 <manraj.engg@gmail.com>
2025-01-06 22:34:32 +0530

    Chess

---
+++ backend1/src/GameManager.ts
@@ -0,0 +1,46 @@
+// interface Game {
+//     id: number;
+//     name: string;
+//     players: WebSocket[];
+// }
+
+import { WebSocket, WebSocketServer } from "ws";
+import { INIT_GAME } from "./messages";
+import { Game } from "./Game";
+// user, game
+export class GameManager {
+    private games: Game[];
+    private pendingUser: WebSocket | null;
+    private users: WebSocket[];
+
+    constructor(){  
+        this.games = [];
+        this.pendingUser = null;
+        this.users = [];
+    }
+
+    addUser (socket: WebSocket){
+         this.users.push(socket);
+         this.addHandler(socket);
+    }
+
+    removerUser (socket: WebSocket){
+        this.users = this.users.filter((user: WebSocket) => user !== socket);
+        //stop game here because user has left
+    }
+    private addHandler(socket:WebSocket) {
+        socket.on("message",(data) => {
+            const message = JSON.parse(data.toString());
+
+            if (message.type === INIT_GAME){
+                if (this.pendingUser){
+                    const game = new Game(this.pendingUser, socket);
+                    this.games.push(game);
+                    this.pendingUser = null;
+                }else{
+                    this.pendingUser = socket;
+                }̀
+            }
+        })
+    }
+}
