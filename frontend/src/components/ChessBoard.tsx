e9ce1013e14fee9ca5b70374d997c2fda4a7e27e
manraj777 <manraj.engg@gmail.com>
2025-01-14 23:16:32 +0530

    connecting player

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -1,18 +1,54 @@
 import { Color, PieceSymbol, Square } from "chess.js";
+import { useState } from "react";
+import { MOVE } from "../screens/Game";
 
-export const ChessBoard = ({ board }: {
+export const ChessBoard = ({ chess, board, socket, setBoard }: {
+    chess: any;
+    setBoard: any;
     board:({
         square: Square;
         type: PieceSymbol;
         color: Color;
     } | null)[][];
+    socket : WebSocket
 }) => {
-    return <div className="text-white-200 w-full">
+    const [from, setFrom] = useState<null | Square>(null);
+    const [to, setTo] = useState<null | Square>(null);
+    return <div className="text-white-200">
         {board.map((row, i) => {
             return <div key={i} className="flex">
                 {row.map((square, j) => {
-                    return <div key={j} className={`w-8 h-8 ${(i+j)%2 ===0 ? 'bg-green-500':'bg-green-300'}`}>
-                        {square ? square.type : ""}
+                    const squareRepresentation = String(65 + (j % 8)) + "" + (8 - i)as Square;
+                    return <div onClick={()=>{
+                        if(!from){
+                            setFrom(squareRepresentation);
+                        } else {
+                            setTo(squareRepresentation);
+                            socket.send(JSON.stringify({
+                                type: MOVE,
+                                payload: {
+                                    from,
+                                    to
+                                }
+                            }))
+
+                            // setFrom(null)
+                            // chess.move({
+                            //     from,
+                            //     to:squareRepresentation
+                            // });
+                            // setBoard(chess.board());
+                            console.log( {
+                                from,
+                                to:squareRepresentation
+                            });
+                        }
+                    }} key={j} className={`w-16 h-16 ${(i+j)%2 ===0 ? 'bg-green-500':'bg-white'}`}>
+                        <div className="w-full justify-center flex h-full">
+                            <div className="h-full justify-center flex flex-col">
+                                {square ? square.type : ""}
+                            </div>
+                        </div>   
                     </div>
                 })}
 
