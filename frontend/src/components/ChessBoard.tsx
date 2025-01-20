b781fd542ce12999b337577c14ebed3bc183f4ba
manraj777 <manraj.engg@gmail.com>
2025-01-20 19:39:08 +0530

    piece move successfully

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -18,35 +18,34 @@ export const ChessBoard = ({ chess, board, socket, setBoard }: {
         {board.map((row, i) => {
             return <div key={i} className="flex">
                 {row.map((square, j) => {
-                    const squareRepresentation = String(65 + (j % 8)) + "" + (8 - i)as Square;
+                    const squareRepresentation = String.fromCharCode(97 + (j % 8)) + "" + (8 - i)as Square;
                     return <div onClick={()=>{
                         if(!from){
                             setFrom(squareRepresentation);
                         } else {
-                            setTo(squareRepresentation);
+                            // setTo(squareRepresentation);
                             socket.send(JSON.stringify({
                                 type: MOVE,
                                 payload: {
                                     move: {
                                         from,
                                         to: squareRepresentation
-                                    }
-                                   
+                                    }                                   
                                 }
                             }))
 
-                            // setFrom(null)
-                            // chess.move({
-                            //     from,
-                            //     to:squareRepresentation
-                            // });
-                            // setBoard(chess.board());
+                            setFrom(null)
+                            chess.move({
+                                from,
+                                to: squareRepresentation
+                            });
+                            setBoard(chess.board());
                             console.log( {
                                 from,
                                 to:squareRepresentation
-                            });
+                            })
                         }
-                    }} key={j} className={`w-16 h-16 ${(i+j)%2 ===0 ? 'bg-green-500':'bg-white'}`}>
+                    }} key={j} className={`w-16 h-16 ${(i+j)%2 === 0 ? 'bg-green-500':'bg-white'}`}>
                         <div className="w-full justify-center flex h-full">
                             <div className="h-full justify-center flex flex-col">
                                 {square ? square.type : ""}
--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -6,7 +6,7 @@ import { useScoket } from "../hooks/useSocket"
 export const INIT_GAME = "init_game";
 export const MOVE = "move";
 export const GAME_OVER = "game_over";
-import { Chess } from 'chess.js'
+import { Chess } from 'chess.js';
 
 
 export const Game = () => {
@@ -22,13 +22,13 @@ export const Game = () => {
             const message = JSON.parse(event.data);
             switch(message.type) {
                 case INIT_GAME:
-                    setChess(new Chess());
+                    // setChess(new Chess());
                     setBoard(chess.board());
                     console.log("Game initialized");
-                    break;
+                    break; 
                 case MOVE:
                     const move = message.payload;
-                    chess .move(move);
+                    chess.move(move);
                     setBoard(chess.board());
                     console.log("Move made");
                     break;
@@ -37,7 +37,7 @@ export const Game = () => {
                     break;
             }
         }
-    },[socket])
+    },[socket, chess]);
     if(!socket) return <div>Conecting...</div>
     return <div className="justify-center flex">
         <div className="pt-8 max-w-screen-lg w-full">
